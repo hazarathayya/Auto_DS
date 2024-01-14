be0fdcf34163f2bbcdac17e569f84fb3fa834679
Hazarath <hazarathayyayallanki@gmail.com>
2024-01-14 14:12:24 +0530

    Added the folder for fucntions in hyperparameter tuning

---
+++ for-hyperparameter-tuning/show_incorrect_predictions.py
@@ -0,0 +1,19 @@
+""" This is a code snippet for showing the images and it's correct label
+ and predicted label in the image classification problem """
+import torch
+import matplotlib.pyplot as plt
+
+def detect_wrong_preds(model, val_loader, dataset):
+  for batch in val_loader:
+    imgs, labels = batch
+    # generate the prediction
+    out = model(imgs) 
+    # get the max probabilty class
+    _, preds = torch.max(out, dim=1)
+    got = preds == labels
+    for i in range(len(got)):
+      if not got[i]:
+        plt.imshow(imgs[i].permute(1, 2, 0).cpu().detach().numpy())
+        plt.show()
+        print('Label:', dataset.classes[labels[i]], ', Predicted:', dataset.classes[preds[i]])
+    break
